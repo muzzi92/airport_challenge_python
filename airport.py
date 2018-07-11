484dc0056a936f2200f990e04d71599cdce86e9d
Muzzi Aldean <amiraldean@gmail.com>
2018-07-11 20:04:13 +0100

    take_off function throws error if weather stormy

--- airport.py
+++ airport.py
@@ -1,8 +1,11 @@
+from weather import Weather
+
 class Airport():
     hanger = []
 
     def land(self, plane):
         self.hanger.append(plane)
 
-    def take_off(self, plane):
+    def take_off(self, plane, weather = Weather().forecast()):
+        if weather == 'stormy' : raise RuntimeError('Cannot take off in storm')
         self.hanger.remove(plane)
--- airport_test.py
+++ airport_test.py
@@ -1,4 +1,5 @@
 from airport import Airport
+import pytest
 
 class TestAirport(object):
 
@@ -15,5 +16,11 @@ class TestAirport(object):
     def test_take_off_removes_from_hanger(self):
         airport = Airport()
         plane = 'plane'
-        airport.take_off(plane)
+        airport.take_off(plane, 'sunny')
         assert plane not in airport.hanger
+
+    def test_does_not_take_off_when_stormy(self):
+        airport = Airport()
+        plane = 'plane'
+        with pytest.raises(RuntimeError):
+            airport.take_off(plane, 'stormy')
